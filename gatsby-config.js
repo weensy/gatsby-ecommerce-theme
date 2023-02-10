3835f8d174cc00590a246709f4b9e154c610152e
weensy <hello@weensy.dev>
2023-02-10 22:33:56 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Gatsby Sydney Ecommerce Theme`,
-        short_name: `Sydney`,
+        short_name: `MONOSEL`,
         start_url: `/`,
         background_color: `#000000`,
         theme_color: `#ffffff`,

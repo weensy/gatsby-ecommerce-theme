b9ee7bf0271c989189fbe5f97e1d1f17528c7ace
weensy <hello@weensy.dev>
2023-02-10 22:48:24 +0900

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -10,7 +10,7 @@ const Brand = (props) => {
       role={'presentation'}
       onClick={() => navigate('/')}
     >
-      <h4>SYDNEY</h4>
+      <h4>MONOSEL</h4>
       {/* <svg
         width={127}
         height={24}
